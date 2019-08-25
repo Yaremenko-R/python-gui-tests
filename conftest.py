f42b94a698c1710eabe5eb1a9d8784b8fa4c6a76
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-25 23:31:14 +0300

    ДЗ24

--- conftest.py
+++ conftest.py
@@ -1,9 +1,26 @@
 import pytest
 from fixture.application import Application
+import importlib
 
 
 @pytest.fixture(scope="session")
 def app(request):
-    fixture = Application("C:\\FreeAddressBookPortable\\AddressBook.exe")
+    testdata = request.config.getoption("--testdata")
+    fixture = Application(target="C:\\FreeAddressBookPortable\\AddressBook.exe", testdata=testdata)
     request.addfinalizer(fixture.destroy)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--testdata", action="store", default="groups.xlsx")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            test_data = load_form_module(fixture[5:])
+            metafunc.parametrize(fixture, test_data, ids=[str(x) for x in test_data])
+
+
+def load_form_module(module):
+    return importlib.import_module("data.%s" % module).test_data
--- fixture/application.py
+++ fixture/application.py
@@ -4,11 +4,12 @@ from fixture.group import GroupHelper
 
 class Application:
 
-    def __init__(self, target):
+    def __init__(self, target, testdata):
         self.application = WinApplication(backend="win32").start(target)
         self.main_window = self.application.window(title="Free Address Book")
         self.main_window.wait("visible")
         self.group = GroupHelper(self)
+        self.testdata = testdata
 
     def destroy(self):
         self.main_window.close()
